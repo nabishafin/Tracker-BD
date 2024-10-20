a22359fc4d1a532207ffee1b2093a21173185856
nabishafin <shafin21215@gmail.com>
2024-10-21 00:45:24 +0600

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <main class="relative mx-auto mt-10 w-full max-w-7xl">
+        <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+
+
+
+
+        </section>
+      </main>
+    </ >
+
+  )
+}
+
+export default App
